0df401d41f393475508b997511dd96e880ace5c0
pigsel <pigsel@yandex.ru>
2020-12-11 23:26:45 +0300

    еще пара строк и уже похоже на чтото

--- centrostruct/centrostruct.py
+++ centrostruct/centrostruct.py
@@ -1,12 +1,9 @@
 # centroids of structures
 # using lidar data of powerline structures to find their axes
 
-# сделать:
-# - варианты без точек - проверить
-# - вынести функции - посмотреть что еще
-# - сделать репорт с выводом на экран !!
-# - вывод в DXF
-
+# TODO - вынести функции - посмотреть что еще
+# TODO - вывод в DXF
+# TODO - интерфейс !
 
 import struct
 import numpy as np
@@ -17,10 +14,11 @@ from shapely.geometry import Point, Polygon
 import csv
 
 # пути
-p = Path(r'/524_wir_16_notime_nocolor.bin')
-p_cgtw = Path(r'/tower331.pts')   # path to ctow
-fin_cgt = Path(r'/cgtow_corr.txt')
-fin_top = Path(r'/tops_corr.txt')
+p = Path('524_wir_16_notime_nocolor.bin')
+p_cgtw = Path('tower331.pts')   # path to ctow
+fin_cgt = Path('cgtow_corr.txt')
+fin_top = Path('tops_corr.txt')
+repr_path = Path('cst_report.txt')
 
 
 # переменные
@@ -33,6 +31,7 @@ bot_str = 30    # процент от высоты опоры снизу для 
 up_str = 10    # процент от высоты опоры сверху для определения центра
 cgtow_corr = []   # обновленные координаты опор середина
 tower_tops = []   # центры верхушек опор середина
+rprt = []   # репорт
 
 
 def bin_reader(p):
@@ -97,11 +96,11 @@ def bin_reader(p):
     return out_header, points
 
 
-def file_write(path):
-    # ну и сохранить в выходные файлы теперь
+def file_write(path, points_arr):
+    # сохранить в текстовые файлы с разделителем пробелом
     with open(path, 'w', newline='') as the_file:
         points_writer = csv.writer(the_file, delimiter=' ')
-        for tup in cgtow_corr:
+        for tup in points_arr:
             points_writer.writerow(tup)
 
 
@@ -111,28 +110,34 @@ def z_level(x, y, g_array, radius):
     return np.mean(grd_cut2['z'])  # новый уровень земли
 
 
+def report(text, rep):
+    print(text)
+    rep.append(text)
+
+
+report('сначала загружаем бин файл', rprt)
+
 p_header, i_points = bin_reader(p)   # вызываем функцию чтения файла
-print(p_header)
 
 # импортные точки переводим в панду и удаляем ненужные колонки
 p_points = pd.DataFrame(i_points, columns=p_header)
 p_points = p_points[['x', 'y', 'z', 'class']]
 del i_points, p_header   # удаляем, больше не нужно
 
-
 # из общего массива точек извлекаем только нужные классы (землю и тела опор)
 grd = p_points[p_points['class'] == grd_class][['x', 'y', 'z']]
 tows = p_points[p_points['class'] == structure_points_class][['x', 'y', 'z']]
 del p_points   # удаляем ненужное
-
+report(f'загружено точек опор: {len(grd)}, земли: {len(tows)}', rprt)
 
 # переводим панду в геопанду
 grd_g = gpd.GeoDataFrame(grd, crs="EPSG:2193", geometry=gpd.points_from_xy(grd['x'], grd['y'], grd['z']))
 tows_g = gpd.GeoDataFrame(tows, crs="EPSG:2193", geometry=gpd.points_from_xy(tows['x'], tows['y'], tows['z']))
-
+report('произведена геопривязка массивов', rprt)
 
 # предварительные координаты опор в панду
 c_tab = pd.read_csv(p_cgtw, sep='\s+', header=None, names=['id', 'x', 'y', 'z'])   # read to pandas
+report(f'загружено исходных координат опор: {len(c_tab)}', rprt)
 
 # теперь умножаем координаты опор на 100
 # т.к. в бинах точки умножены на 100, умножим и тут для простоты
@@ -154,14 +159,16 @@ for n in range(len(cgtw_g)):
     # добавить проверку есть ли чтото в массивах
     if len(tow_cut) == 0 and len(grd_cut) == 0:
         # если не найдено точек опоры и земли оставляем исходные
-        cgt = (n_id, n_z, n_y, n_z)
+        cgt = (n_id, n_x, n_y, n_z)
         top = cgt
+        report(f'опора: {n_id} - оставлены исходные координаты (ТЛО не найдены)', rprt)
 
     elif len(tow_cut) == 0 and len(grd_cut) != 0:
         # если точек от опоры нет, но есть земля - уточняем землю
         grd_lvl = z_level(n_x, n_y, grd_cut, buf_radius_2)
         cgt = (n_id, n_x, n_y, round(grd_lvl / 100, 2))
         top = cgt
+        report(f'опора: {n_id} - уточнена только высота (ТЛО опоры не найдены)', rprt)
 
     elif len(tow_cut) != 0:
         # если есть точки опоры
@@ -169,6 +176,7 @@ for n in range(len(cgtw_g)):
             grd_lvl = np.mean(grd_cut['z'])   # средняя высота земли для расчета примерной высоты опоры
         else:
             grd_lvl = min(tow_cut['z'])   # если нет земли нижняя точка определяется по нижнему отражению опоры
+            report(f'опора: {n_id} - нет ТЛО земли, высота определена по нижнему отражению опоры', rprt)
 
         tow_top = max(tow_cut['z'])   # верхнее отражение от опоры (наивысшая точка)
 
@@ -195,42 +203,24 @@ for n in range(len(cgtw_g)):
         # итоговая координата опоры на верхушке
         top = (n_id, round(up_fig.x / 100, 2), round(up_fig.y / 100, 2), round(tow_top / 100, 2))
 
+        report(f'опора: {n_id} - координаты уточнены', rprt)
+
     else:
         # если ничего не понятно
         cgt = ('error', 0, 0, 0)
         top = cgt
+        report(f'опора: {n_id} - неизвестная ошибка', rprt)
 
     # теперь добавляем полученное в списки
     cgtow_corr.append(cgt)
     tower_tops.append(top)
 
-file_write(fin_cgt)
-file_write(fin_top)
+file_write(fin_cgt, cgtow_corr)
+file_write(fin_top, tower_tops)
+report('записаны выходные файлы: cgtow_corr.txt, tops_corr.txt', rprt)
 
+#  сохраняем репорт на последок
+with open(repr_path, 'w', newline='') as rep_file:
+    csv.writer(rep_file, delimiter='\n').writerow(rprt)
 
-
-####  общая схема и статус:
-# - интерфейс - пути, задание параметров
-# - выбор лас или бин
-# - чтение данных
-# - выборка только нужных классов
-# - поопорный проход циклом
-# - - для каждой опоры:
-# - - вырезаем буфером землю и точки опоры
-# - - проверяем есть ли что нибудь в массивах, если нет то прописываем сами координаты из входного файла
-# - - для определения размера опоры находим предварительный уровень земли и верхнее отражение опоры
-# - - отрезаем из массива опоры верхнюю часть (без Z)
-# - - строим вокруг этих точек полигон, упрощаем его, строим вокруг буфер и находим от этого всего центроид
-# - - тоже от нижней части опоры
-# - - - можно попробовать вырезать низ в пределах верхней траверсы (когда она будет, пока не в этом алгоритме)
-# - - вокруг нижней координаты вырезаем землю поуже и уточняем высоту
-# - - сохраняем координаты верхушки и низа
-# - - ++ добавить построение оси от верха до низа
-# - после прохода по всем опорам экспорт в csv
-# - ++ добавить экспорт осей в dxf (а может и центрлайна тоже)
-
-
-
-
-
-
+input('расчет завершен, нажмите Enter для выхода')
