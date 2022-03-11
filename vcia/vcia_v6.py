bc257c090276b4eb4b80121185177c125df17599
pigsel <pigsel@yandex.ru>
2022-03-11 13:14:48 +0300

    начало

--- vcia/vcia_v6.py
+++ vcia/vcia_v6.py
@@ -19,6 +19,51 @@ for python 3
 
 """
 
+from pathlib import Path
+import openpyxl
+from math import sqrt
+
+# paths to files
+p = Path(r'D:\work\_TP\test\vcia\qgis')  #work dir
+p_str = p / '634_cgtow.pts'
+
+
+def centerline(p_str):
+    # чтение файла координат опор c рабочей нумерацией и запись его в лист c с удалением дубликатов
+    str = []  # list of structures
+    with open(p_str) as ffile:
+        for line in ffile:
+            stroka = line.strip('\n').split()
+            # ctow file should contain work-id, x, y, z in each row
+            if len(stroka) == 4:
+                # check if coords already in list (delete doubles with different names)
+                dub = 0
+                for i in range(len(str)):
+                    # compare x y z written in list
+                    if (str[i][1], str[i][2], str[i][3]) == (float(stroka[1]), float(stroka[2]), float(stroka[3])):
+                        dub +=1
+                if dub < 1:
+                    # if there are no such coords, we can write new one
+                    str.append([int(stroka[0]), float(stroka[1]), float(stroka[2]), float(stroka[3])])
+            elif len(stroka) == 0:
+                # if there is blank stroke in the file
+                print('blank')
+            else:
+                # if stroke looks weird
+                print('ctow file error')
+
+    print(f'загружено опор: {len(str)}')
+    #print(str)
+
+    return str
+
+li_str = centerline(p_str)
+print(li_str[0])
+
+li_str[0].append(9999)
+print(li_str[0])
+
+
 # TODO - чтение спецификации - добавление ид-номеров опор
 # TODO - разбиение на центрлайны (создать лист последовательностей опор - из спецификации)
 # TODO - чтение файлов негабаритов - создание таблицы
@@ -26,9 +71,3 @@ for python 3
 # TODO - интерфейс
 # TODO - таблица под qgis - пролеты с негабаритами, негабаритные точки, длины пролетов
 # TODO - добавить обрезку длинных пролетов
-
-
-from pathlib import Path
-import openpyxl
-from math import sqrt
-
